05a4f921d05d6141f6fe3b955ad0df9493952bb1
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-28 01:32:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// write code here
+function Calculator(){
+  this.num1 = 0
+  this.num2 = 0
+  
+  this.read = function(){
+    this.num1 = +prompt(`Give me a number`)
+    this.num2 = +prompt(`Give me the second number`)
+  }
+  this.sum = function(){
+    return this.num1 + this.num2
+  }
+  this.mul = function(){
+    return this.num1 * this.num2
+  }
+}
+
+let calculator = new Calculator();
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
